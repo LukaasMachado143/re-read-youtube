776442aa7c4f4f46731dadcb3032d67fc65ae944
Lucas Machado <lucas.machado@neogrid.com>
2025-05-23 23:03:45 -0300

    feat: created appbar

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,16 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey.shade600),
+        title: Image.asset("images/youtube.png", width: 100),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.video_call)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.account_circle)),
+        ],
+      ),
+    );
   }
 }
